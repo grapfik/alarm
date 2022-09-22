06e97da54691d397712bd9101566aaeedf90238f
Eris <kali.saint.log@gmail.com>
2022-09-22 17:19:23 -0500

    debug removed; optimized for mobile web

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,11 @@ function App() {
   }
   
   function handleChange(event) {
-    console.log(event.target.value)
     setTimerText(event.target.value)
   }
 
   function handleSubmit(event) {
     event.preventDefault()
-
-    console.log('submit')
 
     function calculateTime() {
       let time = 0
@@ -37,11 +34,7 @@ function App() {
 
       for(let i = parsed.length - 1; i >= 0; i--){
         time += parseInt(parsed[i]) * (60**(parsed.length - i - 1))
-        
-        console.log(time)
       }
-
-      console.log(time)
 
       return time * 1000
     }
